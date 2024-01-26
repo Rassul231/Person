774a318e1e4791207aab5bfb9da189ebf5535572
Rassul231 <153492109+Rassul231@users.noreply.github.com>
2024-01-27 00:27:31 +0600

    Update Main.java

--- src/Work/Main.java
+++ src/Work/Main.java
@@ -1,26 +1,29 @@
 package Work;
 import java.util.ArrayList;
 import java.util.Collections;
-
+// Interface defining a method to get the payment amount.
 interface payable {
     double getPaymentAmount();
 }
 
 public class Main {
+    // Creating a list to store person objects.
     public static void main(String[] args) {
         ArrayList<person> peopleList = new ArrayList<>();
-        peopleList.add(new Employee("Alex", "Sina", "Driver", 320000.00));
+        peopleList.add(new Employee("Winston", "Churchill", "", 9999998.00));
         peopleList.add(new Employee("Rassul", "Satpekov", "Programmer", 9999999.00));
-        peopleList.add(new Student("Norman", "", 1.4));
-        peopleList.add(new Student("Rasmazan", "Horosh", 2.7));
+        peopleList.add(new Student("Alex", "Sina", 1.4));
+        peopleList.add(new Student("Patrick", "Star", 2.7));
+        peopleList.add(new Employee("Berik", "Zhaba", "Drifter", 210000.00));
 
-
+        // Sort each person by they earn
         Collections.sort(peopleList);
 
         printData(peopleList);
     }
 
     public static void printData(Iterable<person> people) {
+        // Printing information about each person from toStrings
         for (person person : people) {
             System.out.println(person.toString());
         }
